523c63b0825d33ea3ad3bf0f077b08ba4352d54c
Huiyu Li <curlyuu@gmail.com>
2021-05-06 23:47:44 +0800

    deploy

---
+++ js/Shopping.e462559d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Shopping"],{e5f6:function(n,e,t){"use strict";t.r(e);var r=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("router-view")],1)},u=[],i={name:"Shopping",data:function(){return{}}},o=i,a=t("2877"),p=Object(a["a"])(o,r,u,!1,null,null,null);e["default"]=p.exports}}]);
+//# sourceMappingURL=Shopping.e462559d.js.map
